40b9b437abe396fe1a8aead1a4baede49f3cdd37
Tom Ordille <thordill@microsoft.com>
2021-06-23 08:01:46 -0400

    second commit

---
+++ Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="TestAzureAD._Default" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+ <dl>
+
+
+  <dt>IsAuthenticated</dt> 
+     <dd><%= HttpContext.Current.User.Identity.IsAuthenticated %></dd>
+     <dt>AuthenticationType</dt> 
+     <dd><%= HttpContext.Current.User.Identity.AuthenticationType %></dd>
+     <dt>Name</dt> <dd><%= HttpContext.Current.User.Identity.Name %></dd>
+     <dt>Is in "Test Group"</dt>
+     <dd><%= HttpContext.Current.User.IsInRole("TestGroup") %></dd> 
+ 
+ </dl>
+
+
+</asp:Content>
---
+++ Global.asax.cs
@@ -0,0 +1,48 @@
+﻿ 
+using System;
+ 
+using System.Linq;
+using System.Security.Claims;
+using System.Threading;
+using System.Web;
+ 
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace TestAzureAD
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+         }
+
+        protected void Application_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+
+        //    if (Request.IsAuthenticated)
+        //    {
+
+        //        ClaimsPrincipal principal = new ClaimsPrincipal(User.Identity);
+
+        //        var identity = (ClaimsIdentity)principal.Identity;
+
+        //        foreach (Claim claim in ClaimsPrincipal.Current.FindAll("groups"))
+        //        {
+        //            var groupStringValue = System.Configuration.ConfigurationManager.AppSettings[claim.Value];
+        //            if (groupStringValue != null)
+        //            {
+        //                identity.AddClaim(new Claim(ClaimTypes.Role, groupStringValue));
+        //            }
+        //        }
+
+        //        Thread.CurrentPrincipal = HttpContext.Current.User = principal;
+        //    }
+
+        }
+
+    }
+}
